9f0cb51531e478dd35f4eb0bbd82096550ded9a4
KarlaMacedo <karlapaolamacedo@gmail.com>
2024-01-24 18:52:14 -0600

    Upload don´t allow non-integer entries

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -17,9 +17,16 @@ public class HumanPlayer extends Player { // Defines HumanPlayer class that inhe
     @Override // This method overrides a method of the base class
     public int makeGuess() { // Defines the method, which requests user's guess and returns the entered value as an integer
         Scanner scanner = new Scanner(System.in); // New scanner integer input tu obtain user data enter
-        System.out.print(name + ", enter a number between 1 and 100 to guess the selected number: "); // Print player name and message to request the guess
-        int guess = scanner.nextInt(); // Gets the guess entered by the user
-        recordGuess(guess); // Records the guess in the history
-        return guess; // Returns entered integer value
+        while (true) { // Enters an infinite loop to keep asking for a number until user enters a valid integer number
+            System.out.print(name + ", enter a number between 1 and 100 to guess the selected number: "); // Print player name and message to request the guess
+            if (scanner.hasNextInt()) { // Checks if the user enter is an integer number
+                int guess = scanner.nextInt(); // Gets the guess entered by the user
+                recordGuess(guess); // Records the guess in the history
+                return guess; // Returns entered integer value and breaks the loop and exitíng the method
+            } else { // if is not an integer number
+                System.out.println("Invalid value. Please enter a ONLY integer numbers."); // Prints an error message
+                scanner.next(); // Cleans the buffer to prevent an infinite loop
+            }
+        }
     }
 }
